52e91a5b559ee376bb81bd780f7e224b04db6f5d
Henry <wilsonhag1@gmail.com>
2026-01-04 17:49:17 -0500

    Actualización: APK compilable y ajustes de Firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -24,7 +24,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.application_peliculas_taller"
+        applicationId = "com.henry.reproductor_peliculas_taller"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
